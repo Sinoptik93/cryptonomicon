3a6e6ce21e5ff104190de6d3f977f1995c1c88b5
Sergey Chepukhin <s.chepukhin@iq-adv.ru>
2021-08-30 17:03:06 +0500

    STEP-1 | Add template elements function

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,40 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-    <div
-      class="
-        fixed
-        w-100
-        h-100
-        opacity-80
-        bg-purple-800
-        inset-0
-        z-50
-        flex
-        items-center
-        justify-center
-      "
-    >
-      <svg
-        class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    </div>
+    <!--    <div-->
+    <!--      class="-->
+    <!--        fixed-->
+    <!--        w-100-->
+    <!--        h-100-->
+    <!--        opacity-80-->
+    <!--        bg-purple-800-->
+    <!--        inset-0-->
+    <!--        z-50-->
+    <!--        flex-->
+    <!--        items-center-->
+    <!--        justify-center-->
+    <!--      "-->
+    <!--    >-->
+    <!--      <svg-->
+    <!--        class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"-->
+    <!--        xmlns="http://www.w3.org/2000/svg"-->
+    <!--        fill="none"-->
+    <!--        viewBox="0 0 24 24"-->
+    <!--      >-->
+    <!--        <circle-->
+    <!--          class="opacity-25"-->
+    <!--          cx="12"-->
+    <!--          cy="12"-->
+    <!--          r="10"-->
+    <!--          stroke="currentColor"-->
+    <!--          stroke-width="4"-->
+    <!--        ></circle>-->
+    <!--        <path-->
+    <!--          class="opacity-75"-->
+    <!--          fill="currentColor"-->
+    <!--          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"-->
+    <!--        ></path>-->
+    <!--      </svg>-->
+    <!--    </div>-->
     <div class="container">
       <section>
         <div class="flex">
@@ -47,6 +47,8 @@
                 type="text"
                 name="wallet"
                 id="wallet"
+                v-model="ticker"
+                @keydown.enter="add"
                 class="
                   block
                   w-full
@@ -133,6 +135,7 @@
         </div>
         <button
           type="button"
+          @click="add"
           class="
             my-4
             inline-flex
@@ -174,8 +177,13 @@
       </section>
 
       <hr class="w-full border-t border-gray-600 my-4" />
-      <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+      <dl
+        v-if="tickers.length"
+        class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3"
+      >
         <div
+          v-for="t of tickers"
+          :key="t.id"
           class="
             bg-white
             overflow-hidden
@@ -187,159 +195,15 @@
         >
           <div class="px-4 py-5 sm:p-6 text-center">
             <dt class="text-sm font-medium text-gray-500 truncate">
-              WTF - USD
+              {{ t.name }} - USD
             </dt>
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">1.11</dd>
+            <dd class="mt-1 text-3xl font-semibold text-gray-900">
+              {{ t.price }}
+            </dd>
           </div>
           <div class="w-full border-t border-gray-200"></div>
           <button
-            class="
-              flex
-              items-center
-              justify-center
-              font-medium
-              w-full
-              bg-gray-100
-              px-4
-              py-4
-              sm:px-6
-              text-md text-gray-500
-              hover:text-gray-600 hover:bg-gray-200 hover:opacity-20
-              transition-all
-              focus:outline-none
-            "
-          >
-            <svg
-              class="h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#718096"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                clip-rule="evenodd"
-              ></path></svg
-            >Удалить
-          </button>
-        </div>
-        <div
-          class="
-            bg-white
-            overflow-hidden
-            shadow
-            rounded-lg
-            border-purple-800 border-solid border-4
-            cursor-pointer
-          "
-        >
-          <div class="px-4 py-5 sm:p-6 text-center">
-            <dt class="text-sm font-medium text-gray-500 truncate">
-              VUE - RUB
-            </dt>
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">80000.00</dd>
-          </div>
-          <div class="w-full border-t border-gray-200"></div>
-          <button
-            class="
-              flex
-              items-center
-              justify-center
-              font-medium
-              w-full
-              bg-gray-100
-              px-4
-              py-4
-              sm:px-6
-              text-md text-gray-500
-              hover:text-gray-600 hover:bg-gray-200 hover:opacity-20
-              transition-all
-              focus:outline-none
-            "
-          >
-            <svg
-              class="h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#718096"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                clip-rule="evenodd"
-              ></path></svg
-            >Удалить
-          </button>
-        </div>
-        <div
-          class="
-            bg-white
-            overflow-hidden
-            shadow
-            rounded-lg
-            border-purple-800 border-solid
-            cursor-pointer
-          "
-        >
-          <div class="px-4 py-5 sm:p-6 text-center">
-            <dt class="text-sm font-medium text-gray-500 truncate">
-              BTC - USD
-            </dt>
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">99999.99</dd>
-          </div>
-          <div class="w-full border-t border-gray-200"></div>
-          <button
-            class="
-              flex
-              items-center
-              justify-center
-              font-medium
-              w-full
-              bg-gray-100
-              px-4
-              py-4
-              sm:px-6
-              text-md text-gray-500
-              hover:text-gray-600 hover:bg-gray-200 hover:opacity-20
-              transition-all
-              focus:outline-none
-            "
-          >
-            <svg
-              class="h-5 w-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="#718096"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                clip-rule="evenodd"
-              ></path></svg
-            >Удалить
-          </button>
-        </div>
-        <div
-          class="
-            bg-white
-            overflow-hidden
-            shadow
-            rounded-lg
-            border-purple-800 border-solid
-            cursor-pointer
-          "
-        >
-          <div class="px-4 py-5 sm:p-6 text-center">
-            <dt class="text-sm font-medium text-gray-500 truncate">
-              DOGE - USD
-            </dt>
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">0.0014</dd>
-          </div>
-          <div class="w-full border-t border-gray-200"></div>
-          <button
+            @click="deleteTicker(t.id)"
             class="
               flex
               items-center
@@ -414,6 +278,35 @@
 <script>
 export default {
   name: "App",
+
+  data() {
+    return {
+      ticker: "",
+      tickers: [
+        { id: 1, name: "DEOM", price: "-" },
+        { id: 2, name: "DSOM", price: "-" },
+        { id: 3, name: "PEOM", price: "-" },
+      ],
+    };
+  },
+  methods: {
+    add() {
+      const newTicker = {
+        id: this.tickers.length,
+        name: this.ticker,
+        price: "-",
+      };
+
+      this.tickers.push(newTicker);
+      this.ticker = "";
+    },
+    deleteTicker(deleteItemId) {
+      this.tickers = this.tickers.filter((t) => t.id !== deleteItemId);
+    },
+    test(evt) {
+      console.log(evt);
+    },
+  },
 };
 </script>
 
